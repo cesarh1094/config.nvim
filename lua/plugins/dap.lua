4be50a2d6bec538dd6e076381a4f494f270edc30
Cesar Hernandez <cesar@mosaicstg.com>
2025-03-04 17:15:57 -0500

    fix: update DAP for PHP

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+      local path = require("mason-registry").get_package("php-debug-adapter"):get_install_path()
+      dap.adapters.php = {
+        type = "executable",
+        command = "node",
+        args = { path .. "/extension/out/phpDebug.js" },
+      }
+
+      dap.configurations.php = {
+        {
+          type = "php",
+          request = "launch",
+          name = "Listen for Xdebug",
+          port = 9000,
+        },
+      }
+    end,
+  },
+}
